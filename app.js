879eaeae62a3ae8f1026d255526d95d5b1f97416
snavadeep <snavadeep@vmware.com>
2019-08-22 02:08:29 +0530

    Craeted the basic NEWS webpage 1. Created html template with styles 2. Created async method for initial rendering 3. Created aysnc source selector 4. Mapped fetching of articles with thhe source

---
+++ app.js
@@ -0,0 +1,42 @@
+const apiKey= '9a12a3c2acd14673abc3ffbe701e94ff';
+const main = document.querySelector('main');
+const sourceSelector = document.querySelector('#sourceSelector');
+const defaultSource =  'the-hindu';
+
+window.addEventListener('load', async e => {
+    updateNews();
+    await updateSources();
+    sourceSelector.value = defaultSource;
+
+    sourceSelector.addEventListener('change', e => {
+        updateNews(e.target.value);
+    });
+});
+
+async function updateSources() {
+    const response = await fetch(`https://newsapi.org/v1/sources`);
+    const json = await response.json();
+
+    sourceSelector.innerHTML = json.sources
+    .map(src => `<option value="${src.id}">${src.name}</option>`);
+}
+
+async function updateNews(source = defaultSource) {
+    const response = await fetch(`https://newsapi.org/v1/articles?source=${source}&apiKey=${apiKey}`);
+    const json = await response.json();
+    
+    
+    main.innerHTML = json.articles.map(createArticle).join('\n');
+}
+
+function createArticle(article) {
+    return `
+    <div class="article">
+        <a href="${article.url}">
+            <h2>${article.title}</h2>
+            </a>
+            <img src="${article.urlToImage}">
+            <p>${article.description}</p>
+    </div>
+    `;
+}
